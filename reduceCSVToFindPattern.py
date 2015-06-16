e434bef57f254876848bed190656c5196fc00764
Jeremiah Marks <Jeremiah@JLMarks.org>
2015-06-15 19:34:59 -0700

    Removed the field names from the patterner so attempt to clean up the display/output a little bit

--- reduceCSVToFindPattern.py
+++ reduceCSVToFindPattern.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # @Author: jeremiah.marks
 # @Date:   2015-06-15 17:46:35
-# @Last Modified by:   jeremiah.marks
-# @Last Modified time: 2015-06-15 18:13:38
+# @Last Modified 2015-06-15
+# @Last Modified time: 2015-06-15 19:27:35
 
 # This script will open a csv file and go through it
 # cell-by-cell, row-by-row and count the number of characters
@@ -17,17 +17,34 @@ import csv
 
 infilename='products.csv'
 outfilename='productsBin.csv'
-
-with open(infilename) as infile:
-    reader=csv.DictReader(infile, restkey="valuesOutsideOfTable")
-    with open(outfilename, 'wb') as outfile:
-        writer=csv.DictWriter(outfile, reader.fieldnames + ["valuesOutsideOfTable"])
-        writer.writeheader()
-        for eachrow in reader:
-            tempthing={}
-            for eachcol in eachrow.keys():
-                if len(eachrow[eachcol])>0:
-                    tempthing[eachcol]="1"
-                else:
-                    tempthing[eachcol]="0"
-            writer.writerow(tempthing)
+def leavefieldnames():
+    with open(infilename) as infile:
+        reader=csv.DictReader(infile, restkey="valuesOutsideOfTable")
+        with open(outfilename, 'wb') as outfile:
+            writer=csv.DictWriter(outfile, reader.fieldnames + ["valuesOutsideOfTable"])
+            writer.writeheader()
+            for eachrow in reader:
+                tempthing={}
+                for eachcol in eachrow.keys():
+                    if len(eachrow[eachcol])>0:
+                        tempthing[eachcol]="1"
+                    else:
+                        tempthing[eachcol]="0"
+                writer.writerow(tempthing)
+def replacefieldnames():
+    with open(infilename) as infile:
+        reader=csv.DictReader(infile, restkey="valuesOutsideOfTable")
+        allfields=reader.fieldnames + ["valuesOutsideOfTable"]
+        for fieldloc, fieldname in enumerate(allfields):
+            allfields[fieldloc]=fieldloc
+        with open(outfilename, 'wb') as outfile:
+            writer=csv.DictWriter(outfile, allfields)
+            writer.writeheader()
+            for eachrow in reader:
+                tempthing={}
+                for eachloc, eachcol in enumerate(eachrow.keys()):
+                    if len(eachrow[eachcol])>0:
+                        tempthing[eachloc]="1"
+                    else:
+                        tempthing[eachloc]="0"
+                writer.writerow(tempthing)
